76a42cbfc35c9f5dfaf47c547344c010ed6a02e0
Yashtripathi9 <145962696+Yashtripathi9@users.noreply.github.com>
2024-07-06 08:13:39 +0530

    firstproject1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const studentNameElement = document.getElementById('studentName');
+    // Replace '[Student Name]' with the actual student name
+    studentNameElement.textContent = 'Yash Tripathi';
+});
